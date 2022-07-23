ac4f07016e270b10c2d3fafa6cc0592d594073d5
Umut Önder <umutemreonder@gmail.com>
2022-07-23 21:50:51 +0300

    Initial Commit

---
+++ src/main/java/org/umutonder/database/Main.java
@@ -0,0 +1,20 @@
+package org.umutonder.database;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-- Before --");
+        CrudOperations.readEmployees();
+
+        System.out.println("\n-- Add Employee --");
+        CrudOperations.addEmployee("Kemal","Buyuk", "Apple");
+
+        System.out.println("\n-- Update Employee --");
+        CrudOperations.updateEmployee(3, "Spotify");
+
+        System.out.println("\n-- Delete Employee --");
+        CrudOperations.deleteEmployee(3);
+
+        System.out.println("\n-- After --");
+        CrudOperations.readEmployees();
+    }
+}
---
+++ src/main/java/org/umutonder/entity/Employee.java
@@ -0,0 +1,31 @@
+package org.umutonder.entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor // Creates a constructor with NonNull variables (nullable = false != NonNull)
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @NonNull
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NonNull
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NonNull
+    @Column(name = "company", nullable = false)
+    private String company;
+}
