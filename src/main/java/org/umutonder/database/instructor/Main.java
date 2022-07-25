eea6ea345d39b93f8a11eb584f210e3582400ae9
Umut Önder <umutemreonder@gmail.com>
2022-07-25 10:07:11 +0300

    Set logger level to off

--- src/main/java/org/umutonder/database/instructor/Main.java
+++ src/main/java/org/umutonder/database/instructor/Main.java
@@ -1,7 +1,12 @@
 package org.umutonder.database.instructor;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args) {
+        Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+
         // CrudOperations.addInstructor("Umut Emre", "Onder", "umutemreonder@gmail.com", "TedeX", "Code"); // Will add instructor detail also. (Cascade)
         // CrudOperations.deleteInstructor(1); // Will delete the instructor detail of the instructor also. (Cascade and OneToOne)
         // CrudOperations.readInstructorDetail(4); // Will print the instructor also. (MappedBy)
@@ -14,7 +19,7 @@ public class Main {
         // CrudOperations.addReview("Java course!", 4);
         // CrudOperations.readReviews(4);
         // CrudOperations.deleteCourse(4); // Will remove the reviews also
-        // CrudOperations.addStudent("Umut Emre", "Onder", "umutemreonder@gmail.com", 2, 3); // Will add the student to the course 2 and 3, also creates rows on class_student table
+        CrudOperations.addStudent("Umut Emre", "Onder", "umutemreonder@gmail.com",  3); // Will add the student to the course 2 and 3, also creates rows on class_student table
         // CrudOperations.addStudent("John", "Doe", "johndoe@gmail.com", 2, 3); // Will add the student to the course 2 and 3, also creates rows on class_student table
         // CrudOperations.readCourses(2); // Prints the info of the course and students
         // CrudOperations.readStudent(2); // Prints the info of the student and courses
